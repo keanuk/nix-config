e2e77ea34d38421cecc761a4d76609e72c83ba7a
Keanu Kerr <keanu.kerr@breadfinancial.com>
2023-08-25 20:40:31 -0500

    Fixed indentations

--- flake.nix
+++ flake.nix
@@ -15,22 +15,22 @@
     cosmic-applets.url = "github:pop-os/cosmic-applets";
     cosmic-applibrary.url = "github:pop-os/cosmic-applibrary";
     cosmic-comp.url = "github:pop-os/cosmic-comp";
-	cosmic-launcher.url = "github:pop-os/cosmic-launcher";
-	cosmic-notifications.url = "github:pop-os/cosmic-notifications";
-	cosmic-osd.url = "github:pop-os/cosmic-osd";
-	cosmic-panel.url = "github:pop-os/cosmic-panel";
-	# cosmic-protocols.url = "github:pop-os/cosmic-protocols";
-	cosmic-settings.url = "github:pop-os/cosmic-settings";
-	cosmic-settings-daemon.url = "github:pop-os/cosmic-settings-daemon";
-	cosmic-session.url = "github:pop-os/cosmic-session";
-	# cosmic-text.url = "github:pop-os/cosmic-text";
-	# cosmic-text-editor.url = "github:pop-os/cosmic-text-editor";
-	# cosmic-theme.url = "github:pop-os/cosmic-theme";
-	# cosmic-theme-editor.url = "github:pop-os/cosmic-theme-editor";
-	# cosmic-time.url = "github:pop-os/cosmic-time";
-	# cosmic-workspaces-epoch.url = "github:pop-os/cosmic-workspaces-epoch";
-	# libcosmic.url = "github:pop-os/libcosmic";
-	xdg-desktop-portal-cosmic.url = "github:pop-os/xdg-desktop-portal-cosmic";
+    cosmic-launcher.url = "github:pop-os/cosmic-launcher";
+    cosmic-notifications.url = "github:pop-os/cosmic-notifications";
+    cosmic-osd.url = "github:pop-os/cosmic-osd";
+    cosmic-panel.url = "github:pop-os/cosmic-panel";
+    # cosmic-protocols.url = "github:pop-os/cosmic-protocols";
+    cosmic-settings.url = "github:pop-os/cosmic-settings";
+    cosmic-settings-daemon.url = "github:pop-os/cosmic-settings-daemon";
+    cosmic-session.url = "github:pop-os/cosmic-session";
+    # cosmic-text.url = "github:pop-os/cosmic-text";
+    # cosmic-text-editor.url = "github:pop-os/cosmic-text-editor";
+    # cosmic-theme.url = "github:pop-os/cosmic-theme";
+    # cosmic-theme-editor.url = "github:pop-os/cosmic-theme-editor";
+    # cosmic-time.url = "github:pop-os/cosmic-time";
+    # cosmic-workspaces-epoch.url = "github:pop-os/cosmic-workspaces-epoch";
+    # libcosmic.url = "github:pop-os/libcosmic";
+    xdg-desktop-portal-cosmic.url = "github:pop-os/xdg-desktop-portal-cosmic";
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
@@ -50,8 +50,8 @@
           ./user/keanu/users.nix
           home-manager.nixosModules.home-manager {
             home-manager.useUserPackages = true;
-		    home-manager.useGlobalPkgs = true;
-		    home-manager.users.keanu = import ./user/keanu/home.nix;
+            home-manager.useGlobalPkgs = true;
+            home-manager.users.keanu = import ./user/keanu/home.nix;
           }
         ];
       };
