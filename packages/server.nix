3663ff6fbf924cce3cafe786099707b0ecc71d83
Keanu Kerr <keanu.kerr@breadfinancial.com>
2023-09-25 21:07:36 -0500

    Added fonts to server

--- packages/server.nix
+++ packages/server.nix
@@ -2,6 +2,13 @@
 
 {
   environment.systemPackages = with pkgs; [
+      font-awesome
+      nerdfonts
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      powerline-fonts
+      source-code-pro
   ];
 
   users.users.keanu.packages = with pkgs; [
