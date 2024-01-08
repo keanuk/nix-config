8f2711e3e4e4f0cd83e25d3914987919ee78627d
Keanu Kerr <keanu.kerr@breadfinancial.com>
2024-01-08 17:42:32 -0600

    Removed COSMIC flakes

--- flake.nix
+++ flake.nix
@@ -4,11 +4,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     stable.url = "github:NixOS/nixpkgs/nixos-23.11";
-
-    # Nix hardware
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     
-    # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";	
@@ -19,94 +16,32 @@
       inputs.nixpkgs.follows = "stable";	
     };
 
-    # Secure Boot
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # VSCode Server
-    vscode-server = {
-      url = "github:nix-community/nixos-vscode-server";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # COSMIC Desktop
-    cosmic-applets = {
-      url = "github:pop-os/cosmic-applets";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    cosmic-applibrary = {
-      url = "github:pop-os/cosmic-applibrary";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    cosmic-bg = {
-      url = "github:pop-os/cosmic-bg";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    cosmic-comp = {
-      url = "github:pop-os/cosmic-comp";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    cosmic-launcher = {
-      url = "github:pop-os/cosmic-launcher";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    cosmic-notifications = {
-      url = "github:pop-os/cosmic-notifications";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    cosmic-osd = {
-      url = "github:pop-os/cosmic-osd";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    cosmic-panel = {
-      url = "github:pop-os/cosmic-panel";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    # cosmic-protocols.url = "github:pop-os/cosmic-protocols";
-    
-    cosmic-settings = {
-      url = "github:pop-os/cosmic-settings";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    cosmic-settings-daemon = {
-      url = "github:pop-os/cosmic-settings-daemon";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    cosmic-session = {
-      url = "github:pop-os/cosmic-session";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    # cosmic-text.url = "github:pop-os/cosmic-text";
-    # cosmic-text-editor.url = "github:pop-os/cosmic-text-editor";
-    # cosmic-theme.url = "github:pop-os/cosmic-theme";
-    # cosmic-theme-editor.url = "github:pop-os/cosmic-theme-editor";
-    # cosmic-time.url = "github:pop-os/cosmic-time";
-    # cosmic-workspaces-epoch.url = "github:pop-os/cosmic-workspaces-epoch";
-    # libcosmic.url = "github:pop-os/libcosmic";
-    
-    xdg-desktop-portal-cosmic = {
-      url = "github:pop-os/xdg-desktop-portal-cosmic";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
+    };    
   };
 
-  outputs = inputs@{ self, nixpkgs, stable, nixos-hardware, home-manager, home-manager-stable, lanzaboote, vscode-server, 
-    cosmic-applets, cosmic-applibrary, cosmic-bg, cosmic-comp, cosmic-launcher, cosmic-notifications, cosmic-osd,
-    cosmic-panel, cosmic-settings, cosmic-settings-daemon, cosmic-session, xdg-desktop-portal-cosmic, ... }: {
+  outputs = { 
+    self, 
+    nixpkgs, 
+    stable, 
+    nixos-hardware, 
+    home-manager, 
+    home-manager-stable, 
+    lanzaboote, 
+    ... 
+  } @ inputs: let
+    inherit (self) outputs;
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ]
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
     nixosConfigurations = {
       earth = stable.lib.nixosSystem {
         system = "x86_64-linux";
@@ -179,7 +114,6 @@
             };
           }
           lanzaboote.nixosModules.lanzaboote
-          vscode-server.nixosModules.default
         ];
       };
       hermes = nixpkgs.lib.nixosSystem {
@@ -216,7 +150,6 @@
             };
           }
           lanzaboote.nixosModules.lanzaboote
-          vscode-server.nixosModules.default
         ];
       };
       terra = stable.lib.nixosSystem {
@@ -289,7 +222,6 @@
             };
           }
           lanzaboote.nixosModules.lanzaboote
-          vscode-server.nixosModules.default
         ];
       };
     };
