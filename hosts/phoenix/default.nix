964e2004bf5da04562aff1d19e9682b24f91b542
Keanu Kerr <keanu@kerr.us>
2024-01-14 11:55:14 -0600

    nix darwin configs

--- hosts/phoenix/default.nix
+++ hosts/phoenix/default.nix
@@ -2,19 +2,26 @@
 	imports = [
 		./hardware-configuration.nix
 
-    inputs.home-manager.nixosModules.home-manager
+    # inputs.home-manager.nixosModules.home-manager
+		../common-darwin/nix/configuration.nix
 	];
 
   networking.hostName = "phoenix";
 
-  home-manager = {
-    extraSpecialArgs = { inherit inputs outputs; };
-    useUserPackages = true;
-    useGlobalPkgs = true;
-    users.keanu = {
-      imports = [ 
-      ];
-      home.stateVersion = "24.05";
-    };
-  };
+  # home-manager = {
+  #   extraSpecialArgs = { inherit inputs outputs; };
+  #   useUserPackages = true;
+  #   useGlobalPkgs = true;
+  #   users.keanu = {
+  #     imports = [ 
+  #     ];
+  #     home.stateVersion = "24.05";
+  #   };
+  # };
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
+
+	system.stateVersion = 4;
 }
