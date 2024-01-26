249a09df91658bf6bb1bcfe624870f37ac63e8c5
Keanu Kerr <keanu@kerr.us>
2024-01-25 20:45:39 -0500

    Added COSMIC DE components

--- hosts/common/nixos/desktop/cosmic.nix
+++ hosts/common/nixos/desktop/cosmic.nix
@@ -3,15 +3,24 @@
 {
   environment.systemPackages = with pkgs; [
     cosmic-applets
+    cosmic-applibrary
+    cosmic-bg
     cosmic-comp
     cosmic-edit
+    cosmic-files
     cosmic-greeter
     cosmic-icons
     cosmic-launcher
+    cosmic-notifications
     cosmic-osd
     cosmic-panel
+    cosmic-protocols
+    cosmic-randr
+    cosmic-screenshot
     cosmic-settings
+    cosmic-settings-daemon
     cosmic-term
     cosmic-workspaces-epoch
+    xdg-desktop-portal-cosmic
   ];
 }
