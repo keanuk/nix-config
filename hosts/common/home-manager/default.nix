feda0690398d534f7ee5a6de8e94eba37d1fe8d5
Keanu Kerr <keanu@kerr.us>
2024-02-25 19:56:06 -0500

    Disable nextDNS in resolved and add offline rebuild alias

--- hosts/common/home-manager/default.nix
+++ hosts/common/home-manager/default.nix
@@ -28,6 +28,7 @@
         cleanup = "nix-store --gc && nix-store --optimize";
         ll = "ls -l";
         rebuild = "sudo nixos-rebuild switch --upgrade";
+        rebuild-offline = "sudo nixos-rebuild switch --option substitute false";
         repair = "sudo nix-store --verify --check-contents --repair";
         update = "nix flake update ~/.config/nix-config";
       };
--- hosts/common/nixos/system/default.nix
+++ hosts/common/nixos/system/default.nix
@@ -36,13 +36,13 @@
     power-profiles-daemon.enable = true;
     resolved = { 
       enable = true;
-      extraConfig = ''
-        DNS=45.90.28.0#${secrets.nextdns.id}.dns.nextdns.io
-        DNS=2a07:a8c0::#${secrets.nextdns.id}.dns.nextdns.io
-        DNS=45.90.30.0#${secrets.nextdns.id}.dns.nextdns.io
-        DNS=2a07:a8c1::#${secrets.nextdns.id}.dns.nextdns.io
-        DNSOverTLS=yes
-      '';
+    #  extraConfig = ''
+    #    DNS=45.90.28.0#${secrets.nextdns.id}.dns.nextdns.io
+    #    DNS=2a07:a8c0::#${secrets.nextdns.id}.dns.nextdns.io
+    #    DNS=45.90.30.0#${secrets.nextdns.id}.dns.nextdns.io
+    #    DNS=2a07:a8c1::#${secrets.nextdns.id}.dns.nextdns.io
+    #    DNSOverTLS=yes
+    #  '';
     };
     smartd.enable = true;
     tailscale.enable = true;
