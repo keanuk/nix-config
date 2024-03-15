1138cff787b1d4653a7fa00af3035e4b94c37a8f
Keanu Kerr <keanu@kerr.us>
2024-03-15 18:06:58 -0500

    updated flake

--- hosts/common/nixos/services/nixarr.nix
+++ hosts/common/nixos/services/nixarr.nix
@@ -11,18 +11,21 @@
     mediaDir = "/internal/media";
     stateDir = "/var/lib/nixarr";
 
-    vpn = {
-      enable = true;
-      wgConf = "/home/keanu/.secrets/wg0.conf";
-    };
+    # vpn = {
+    #   enable = true;
+    #   wgConf = "/home/keanu/.secrets/wg0.conf";
+    # };
 
     transmission = {
       enable = true;
       openFirewall = true;
       # vpn.enable = true;
-      peerPort = 50000;
+      # peerPort = 50000;
       uiPort = 9091;
-      flood.enable = true;
+      # flood.enable = true;
+      extraAllowedIps = [
+        "10.19.5.10"
+      ];
     };
 
     jellyfin = {
