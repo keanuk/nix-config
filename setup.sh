6ba32863f09eb56ce00ca3a4276d050e8467e846
Keanu Kerr <keanu@kerr.us>
2024-04-03 00:43:13 -0500

    setup no longer preserves existing config files

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Backup original configuration.nix and hardware-configuration.nix
-mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
-mv /etc/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix.bak
-
 # Delete existing symlinks
 find /etc/nixos/* ! -name '*.bak' -type l -delete
 
