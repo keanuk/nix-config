505865e349e2f44387414a125684772f88ffeb10
Keanu Kerr <keanu@kerr.us>
2024-04-28 11:12:59 -0400

    renamed libinput

--- hosts/common/nixos/desktop/default.nix
+++ hosts/common/nixos/desktop/default.nix
@@ -20,7 +20,7 @@
 
   services.flatpak.enable = true;
   services.dbus.enable = true;
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   # Fingerprint login
