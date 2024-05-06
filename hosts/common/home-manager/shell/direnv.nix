7388486e588386e456c10a82b58a950cc8e19b62
Keanu Kerr <keanu@kerr.us>
2024-05-06 13:19:42 -0400

    diabled fish integration for direnv

--- hosts/common/home-manager/shell/direnv.nix
+++ hosts/common/home-manager/shell/direnv.nix
@@ -4,7 +4,7 @@
   programs.direnv = {
     enable = true;
     enableBashIntegration = true;
-    enableFishIntegration = true;
+    # enableFishIntegration = true;
     enableNushellIntegration = true;
     enableZshIntegration = true;
   };
