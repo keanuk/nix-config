315853e716fd4749855bf2dc5939267ec8b80a3c
Keanu Kerr <keanu@kerr.us>
2024-05-07 17:16:05 -0400

    added nushell aliases

--- hosts/common/home-manager/shell/nushell.nix
+++ hosts/common/home-manager/shell/nushell.nix
@@ -12,18 +12,47 @@
         }
       '';
     };
-    loginFile = {
-      text = ''
-        fastfetch
-      '';
-    };
+    extraConfig = ''
+      let carapace_completer = {|spans|
+        carapace $spans.0 nushell $spans | from json
+      }
+      $env.config = {
+        show_banner: false,
+        completions: {
+          case_sensitive: false
+          quick: true
+          partial: true
+          algorithm: "fuzzy"
+          external: {
+            enable: true 
+            max_results: 100 
+            completer: $carapace_completer
+          }
+        }
+      } 
+      $env.PATH = ($env.PATH | 
+      split row (char esep) |
+      prepend /home/myuser/.apps |
+      append /usr/bin/env
+      )
+      fastfetch
+    '';
     shellAliases = {
-      cleanup = "nix-store --gc && nix-store --optimize";
-      ll = "ls -l";
+      cat = "bat";
+      cd = "z";
+      cleanup = "nix-store --gc and nix-store --optimize";
+      find = "fd";
+      grep = "rg";
+      ll = "eza -l";
+      ls = "eza";
+      nano = "micro";
       rebuild = "sudo nixos-rebuild switch --upgrade";
       rebuild-offline = "sudo nixos-rebuild switch --option substitute false";
       repair = "sudo nix-store --verify --check-contents --repair";
+      top = "btm";
       update = "nix flake update ~/.config/nix-config";
+      vi = "hx";
+      vim = "nvim";
     };
   };
 }
