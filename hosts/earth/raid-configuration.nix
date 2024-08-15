242e8e945a3ecd88f9ae2dc8150c4f293e50363f
Keanu Kerr <keanu@kerr.us>
2024-08-15 13:07:19 -0400

    updated flake

--- hosts/earth/raid-configuration.nix
+++ hosts/earth/raid-configuration.nix
@@ -1,5 +1,9 @@
 { ... }:
 
 {
-  
+  systemd.services.mount-raid = {
+    description = "Mount RAID configuration";
+    script = /run/current-system/sw/bin/bcachefs ;
+    wantedBy = ["multi-user.target"];
+  };
 }
