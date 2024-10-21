011a365e32508d96a63c424c19312a34eb40e709
Keanu Kerr <keanu@kerr.us>
2024-10-21 07:03:49 -0400

    disabled hyprbars

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -47,7 +47,7 @@
     };
     xwayland.enable = true;
     plugins = [
-      inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
+      # inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
       # inputs.hyprland-plugins.packages.${pkgs.system}.hyprexpo
       # inputs.hyprland-plugins.packages.${pkgs.system}.hyprtrails
     ];
