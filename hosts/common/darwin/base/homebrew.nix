6b8bf714020be45845c155cab9dfc030cb29f4bf
Keanu Kerr <keanu@kerr.us>
2024-11-19 01:08:21 -0500

    added brew casks

--- hosts/common/darwin/base/homebrew.nix
+++ hosts/common/darwin/base/homebrew.nix
@@ -3,15 +3,27 @@
 {
   homebrew = {
     enable = true;
-    taps = [];
+    taps = [
+      "homebrew/cask"
+      "homebrew/core"
+    ];
     brews = [];
     casks = [
       "beeper"
+      "chromium"
+      "firefox"
       "firefox@developer-edition"
+      "github@beta"
       "iterm2@beta"
       "proton-mail"
       "protonmail-bridge"
+      "standard-notes"
+      "thunderbird"
       "thunderbird@beta"
+      "visual-studio-code"
+      "visual-studio-code@insiders"
+      "zed"
+      "zed@preview"
     ];
   };
 }
