dba1b7aa3e4ff77a9bec39fb840916578ab94327
Keanu Kerr <keanu@kerr.us>
2024-11-18 23:51:25 -0500

    added homebrew casks

--- hosts/common/darwin/base/default.nix
+++ hosts/common/darwin/base/default.nix
@@ -20,6 +20,26 @@
     settings = {
       allowed-users = [ "@users" "keanu" "root" ];
       experimental-features = [ "nix-command" "flakes" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://keanu.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://cosmic.cachix.org"
+        "https://nix-gaming.cachix.org"
+      ];
+      trusted-public-keys = [
+        "keanu.cachix.org-1:bnYEu6tJzXfwM5JkEhc90uEjR7cAHwaa4fwHRCYdFGg="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      ];
+      trusted-users = [
+        "root"
+        "keanu"
+        "@wheel"
+      ];
     };
   };
   programs.zsh.enable = true;
--- hosts/common/darwin/base/homebrew.nix
+++ hosts/common/darwin/base/homebrew.nix
@@ -5,6 +5,13 @@
     enable = true;
     taps = [];
     brews = [];
-    casks = [ "beeper" ];
+    casks = [
+      "beeper"
+      "firefox@developer-edition"
+      "iterm2@beta"
+      "proton-mail"
+      "protonmail-bridge"
+      "thunderbird@beta"
+    ];
   };
 }
