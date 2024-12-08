4fd260a8c94a0bad739dda810b235d5ca54b69f4
Keanu Kerr <keanu@kerr.us>
2024-12-08 15:55:51 -0600

    added fixes for sonarr

--- hosts/common/nixos/desktop/packages.nix
+++ hosts/common/nixos/desktop/packages.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }:
 
 {
-  nixpkgs.config.permittedInsecurePackages = [ ];
-
   environment = {
     sessionVariables = { };
     systemPackages = with pkgs; [
--- overlays/default.nix
+++ overlays/default.nix
@@ -9,6 +9,13 @@
     unstable = import inputs.nixpkgs {
       system = final.system;
       config.allowUnfree = true;
+
+      config.permittedInsecurePackages = [
+        "aspnetcore-runtime-6.0.36"
+        "aspnetcore-runtime-wrapped-6.0.36"
+        "dotnet-sdk-6.0.428"
+        "dotnet-sdk-wrapped-6.0.428"
+      ];
     };
   };
 }
