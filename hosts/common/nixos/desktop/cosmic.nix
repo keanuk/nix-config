1af32313268d3cc0a4adcf5769a035f1bda893b7
Keanu Kerr <keanu@kerr.us>
2024-12-14 13:45:47 -0500

    removed individual cosmic packages

--- hosts/common/nixos/desktop/cosmic.nix
+++ hosts/common/nixos/desktop/cosmic.nix
@@ -1,32 +1,6 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
-
-  # environment.systemPackages = with pkgs; [
-  #   cosmic-applets
-  #   cosmic-applibrary
-  #   cosmic-bg
-  #   cosmic-comp
-  #   cosmic-edit
-  #   cosmic-files
-  #   cosmic-greeter
-  #   cosmic-icons
-  #   cosmic-launcher
-  #   cosmic-notifications
-  #   cosmic-osd
-  #   cosmic-panel
-  #   cosmic-protocols
-  #   cosmic-randr
-  #   cosmic-screenshot
-  #   cosmic-session
-  #   cosmic-settings
-  #   cosmic-settings-daemon
-  #   cosmic-store
-  #   cosmic-term
-  #   cosmic-wallpapers
-  #   cosmic-workspaces-epoch
-  #   xdg-desktop-portal-cosmic
-  # ];
 }
