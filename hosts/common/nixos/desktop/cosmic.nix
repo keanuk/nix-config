9fa3ed94f6eca1c28e052f366581d1be11c5c87a
Keanu Kerr <keanu@kerr.us>
2024-12-14 13:44:26 -0500

    update

--- hosts/common/nixos/desktop/cosmic.nix
+++ hosts/common/nixos/desktop/cosmic.nix
@@ -4,29 +4,29 @@
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    cosmic-applets
-    cosmic-applibrary
-    cosmic-bg
-    cosmic-comp
-    cosmic-edit
-    cosmic-files
-    cosmic-greeter
-    cosmic-icons
-    cosmic-launcher
-    cosmic-notifications
-    cosmic-osd
-    cosmic-panel
-    cosmic-protocols
-    cosmic-randr
-    cosmic-screenshot
-    cosmic-session
-    cosmic-settings
-    cosmic-settings-daemon
-    cosmic-store
-    cosmic-term
-    cosmic-wallpapers
-    cosmic-workspaces-epoch
-    xdg-desktop-portal-cosmic
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   cosmic-applets
+  #   cosmic-applibrary
+  #   cosmic-bg
+  #   cosmic-comp
+  #   cosmic-edit
+  #   cosmic-files
+  #   cosmic-greeter
+  #   cosmic-icons
+  #   cosmic-launcher
+  #   cosmic-notifications
+  #   cosmic-osd
+  #   cosmic-panel
+  #   cosmic-protocols
+  #   cosmic-randr
+  #   cosmic-screenshot
+  #   cosmic-session
+  #   cosmic-settings
+  #   cosmic-settings-daemon
+  #   cosmic-store
+  #   cosmic-term
+  #   cosmic-wallpapers
+  #   cosmic-workspaces-epoch
+  #   xdg-desktop-portal-cosmic
+  # ];
 }
