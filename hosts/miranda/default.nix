5fc531ad1f7999f17f2e2ba7a49bfd593b39ebc5
Keanu Kerr <keanu@kerr.us>
2025-02-02 23:35:45 +0000

    Update default.nix

--- hosts/miranda/default.nix
+++ hosts/miranda/default.nix
@@ -1,15 +1,16 @@
 { inputs, outputs, nix-colors, ... }: {
   imports = [
     ./hardware-configuration.nix
+    ./disko-configuration.nix
 
     inputs.home-manager.nixosModules.home-manager
     inputs.nur.modules.nixos.default
     ../common/nixos/base/default.nix
-    ../common/nixos/base/ephemeral-boot-luks-btrfs.nix
-    ../common/nixos/base/impermanence.nix
+    # ../common/nixos/base/ephemeral-boot-luks-btrfs.nix
+    # ../common/nixos/base/impermanence.nix
     ../common/nixos/base/lanzaboote.nix
     ../common/nixos/desktop/default.nix
-    ../common/nixos/desktop/cosmic.nix
+    ../common/nixos/desktop/gnome.nix
     ../common/nixos/desktop/hyprland.nix
     ../common/nixos/user/keanu/default.nix
 
