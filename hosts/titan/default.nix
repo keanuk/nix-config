fb5f2f55a827d6bf68af0f79e223f1731c22b17f
Keanu Kerr <keanu@kerr.us>
2025-02-24 18:19:25 -0500

    switched titan to pantheon

--- hosts/titan/default.nix
+++ hosts/titan/default.nix
@@ -11,7 +11,7 @@
     ../common/nixos/base/lanzaboote.nix
     ../common/nixos/desktop/default.nix
     ../common/nixos/desktop/hyprland.nix
-    ../common/nixos/desktop/gnome.nix
+    ../common/nixos/desktop/pantheon.nix
     ../common/nixos/user/keanu/default.nix
 
     # Change during next reinstall
