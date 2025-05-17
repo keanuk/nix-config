cccb82d51a548565dedfc5cbf7539b684619a4fc
Keanu Kerr <keanu@kerr.us>
2025-05-17 11:02:22 -0400

    Create aliases.nix

---
+++ home/common/shell/config/aliases.nix
@@ -0,0 +1,20 @@
+{
+  cat = "bat";
+  cd = "z";
+  cleanup = "nix-store --gc";
+  find = "fd";
+  grep = "rg";
+  ll = "eza -l";
+  ls = "eza";
+  nano = "micro";
+  optimize = "nix-store --optimize";
+  rebuild = "sudo nixos-rebuild switch --upgrade --flake ~/.config/nix-config";
+  rebuild-darwin = "darwin-rebuild switch --flake ~/.config/nix-config";
+  rebuild-offline = "sudo nixos-rebuild switch --option substitute false";
+  repair = "sudo nix-store --verify --check-contents --repair";
+  top = "btm";
+  update = "nix flake update --flake ~/.config/nix-config";
+  update-darwin = "nix flake update --flake /Users/keanu/.config/nix-config";
+  vi = "hx";
+  vim = "nvim";
+}
