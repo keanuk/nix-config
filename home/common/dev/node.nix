d35b989f398b1714aa5522fffe02eab8d029b2df
Keanu Kerr <keanu@kerr.us>
2025-05-28 23:02:04 -0400

    add some node package managers

--- home/common/dev/node.nix
+++ home/common/dev/node.nix
@@ -2,7 +2,11 @@
   home.packages = with pkgs; [
     angular-language-server
     autoprefixer
+    bun
+    deno
+    eslint
     htmx-lsp2
+    pnpm
     svelte-language-server
     tailwindcss_4
     tailwindcss-language-server
@@ -12,6 +16,7 @@
     nodePackages_latest.nodejs
     # TODO: enable when the build succeeds
     # nodePackages_latest.postcss
+    nodePackages.prettier
     nodePackages_latest.svelte-check
   ];
 }
