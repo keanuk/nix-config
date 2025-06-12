d2d1a9c1bd3cd77769a22dc80e35e38c4f3cfd6e
Keanu Kerr <keanu@kerr.us>
2025-06-12 05:28:06 -0400

    rmeove outdated workaounds

--- hosts/common/nixos/services/flaresolverr.nix
+++ hosts/common/nixos/services/flaresolverr.nix
@@ -2,8 +2,6 @@
   services.flaresolverr = {
     enable = true;
     openFirewall = true;
-    # TODO switch back to pkgs.flaresolverr when the package is fixed
-    # https://github.com/NixOS/nixpkgs/issues/332776
-    package = pkgs.nur.repos.xddxdd.flaresolverr-21hsmw;
+    package = pkgs.unstable.flaresolverr;
   };
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -7,14 +7,6 @@
     unstable = import inputs.nixpkgs {
       system = final.system;
       config.allowUnfree = true;
-
-      # TODO remove when the arrs no longer use .NET 6
-      config.permittedInsecurePackages = [
-        "aspnetcore-runtime-6.0.36"
-        "aspnetcore-runtime-wrapped-6.0.36"
-        "dotnet-sdk-6.0.428"
-        "dotnet-sdk-wrapped-6.0.428"
-      ];
     };
   };
 
