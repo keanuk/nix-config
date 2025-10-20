ab6c93b7e5028cc3f8c7205543aac4da5314667b
Keanu Kerr <keanu@kerr.us>
2025-10-20 18:22:33 -0400

    reverted to older git config until 25.11 release

--- home/_mixins/shell/git/default.nix
+++ home/_mixins/shell/git/default.nix
@@ -2,23 +2,40 @@
   programs.git = {
     enable = true;
     package = pkgs.git;
+    userName = "Keanu Kerr";
+    userEmail = "keanu@kerr.us";
     lfs.enable = true;
-    settings = {
-      user = {
-        name = "Keanu Kerr";
-        email = "keanu@kerr.us";
-      };
+    delta = {
+      enable = true;
+    };
+    extraConfig = {
       github.user = "keanuk";
       init.defaultBranch = "main";
       pull.rebase = false;
     };
   };
 
-  programs.delta = {
-    enable = true;
-    options = {
-      navigate = true;
-      side-by-side = true;
-    };
-  };
+  # TODO: Switch to this config when 25.11 is released
+  # programs.git = {
+  #   enable = true;
+  #   package = pkgs.git;
+  #   lfs.enable = true;
+  #   settings = {
+  #     user = {
+  #       name = "Keanu Kerr";
+  #       email = "keanu@kerr.us";
+  #     };
+  #     github.user = "keanuk";
+  #     init.defaultBranch = "main";
+  #     pull.rebase = false;
+  #   };
+  # };
+
+  # programs.delta = {
+  #   enable = true;
+  #   options = {
+  #     navigate = true;
+  #     side-by-side = true;
+  #   };
+  # };
 }
