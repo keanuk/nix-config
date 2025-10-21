cfecc5f801f83822225ebb97b3f5a3cb3899e711
Keanu Kerr <keanu@kerr.us>
2025-10-21 18:23:08 -0400

    added home assistant settings

--- nixos/_mixins/services/home-assistant/default.nix
+++ nixos/_mixins/services/home-assistant/default.nix
@@ -4,11 +4,21 @@
     package = pkgs.unstable.home-assistant;
     openFirewall = true;
     config = {
+      default_config = {};
       homeassistant = {
         name = "Home";
         unit_system = "metric";
         time_zone = "EST";  
       };
     };
+    extraComponents = [
+      "analytics"
+      "google_translate"
+      "isal"
+      "met"
+      "mobile_app"
+      "radio_browser"
+      "shopping_list"
+    ];
   };
 }
--- nixos/phoebe/default.nix
+++ nixos/phoebe/default.nix
@@ -26,7 +26,6 @@
 
     ../_mixins/services/btrfs/default.nix
     ../_mixins/services/ollama/default.nix
-    ../_mixins/services/open-webui/default.nix
 
     ../_mixins/user/keanu/default.nix
   ];
