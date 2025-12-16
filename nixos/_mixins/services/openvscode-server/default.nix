78647b9ff31a739a3ec134fc94aac6cb7e835ad0
Keanu Kerr <keanu@kerr.us>
2025-12-15 23:41:18 -0500

    run openvscode server as user

--- nixos/_mixins/services/openvscode-server/default.nix
+++ nixos/_mixins/services/openvscode-server/default.nix
@@ -2,7 +2,9 @@
   services.openvscode-server = {
     enable = true;
     package = pkgs.unstable.openvscode-server;
-    serverDataDir = "/opt/openvscode-server";
+    user = "keanu";
+    group = "users";
+    serverDataDir = "/home/keanu/.openvscode-server";
     port = 3000;
     host = "127.0.0.1";
     # Disable connection token since we're protecting access with Authelia 2FA
