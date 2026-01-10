f1ec8a7384a7536f7e23282f2bdb5f9871cb30c6
Keanu Kerr <keanu@kerr.us>
2026-01-10 17:21:29 -0500

    add public keys to keanu

--- nixos/_mixins/user/keanu/default.nix
+++ nixos/_mixins/user/keanu/default.nix
@@ -14,7 +14,9 @@
     shell = pkgs.fish;
     initialPassword = "keanu";
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIICP+F1NN7ilYQdrDouylmVIJK3szsurUSl/ZtTWB2rE keanu@beehive"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINWMzYrXSIYdnLCK+Tvg37gZtKhRimeo/0KuGc39jWen keanu@salacia"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFMejX8oSoN9Jp4HQcyGj9PZoLz30w3hty2lw6vWv5ab keanu@titan"
 
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDS5rawgaqa78S0s07aGlWgvHrVzb3QzUocqq51u0od3 github-action-bucaccio"
     ];
