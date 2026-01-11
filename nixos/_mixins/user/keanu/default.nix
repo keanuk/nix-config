96c8f06c591bcd2595d39c9e2132f8289109f332
Keanu Kerr <keanu@kerr.us>
2026-01-11 00:21:25 -0500

    add vesta ssh key

--- nixos/_mixins/user/keanu/default.nix
+++ nixos/_mixins/user/keanu/default.nix
@@ -20,6 +20,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOYuGM8TymDA4CwoFJUo2jJv0kVFYxEBOHmCuTgGcwaa keanu@phoebe"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINWMzYrXSIYdnLCK+Tvg37gZtKhRimeo/0KuGc39jWen keanu@salacia"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFMejX8oSoN9Jp4HQcyGj9PZoLz30w3hty2lw6vWv5ab keanu@titan"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERTrzal23ssMxWgUfDJxeWYfwAbRcJ6zDeDNKsQu9Ag keanu@vesta"
 
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDS5rawgaqa78S0s07aGlWgvHrVzb3QzUocqq51u0od3 github-action-bucaccio"
     ];
