b3e43104b740f4edaab89b66b84b4970bba1673a
Keanu Kerr <keanu@kerr.us>
2026-02-02 20:19:03 -0500

    disable nushell tests in bat-extras overlay

--- overlays/fixes/bat-extras.nix
+++ overlays/fixes/bat-extras.nix
@@ -1,21 +1,22 @@
 # Disable checks for bat-extras packages to avoid nushell dependency
 # Issue: bat-extras tests require nushell as nativeCheckInputs
-# Workaround: Disable doCheck for affected packages
+# Workaround: Override the entire bat-extras scope to pass a dummy nushell
 # Status: temporary - nushell build fails on darwin
 # Last checked: 2026-02-02
 _final: prev: {
-  bat-extras = prev.bat-extras // {
-    batdiff = prev.bat-extras.batdiff.overrideAttrs (_oldAttrs: {
-      doCheck = false;
-    });
-    batgrep = prev.bat-extras.batgrep.overrideAttrs (_oldAttrs: {
-      doCheck = false;
-    });
-    batman = prev.bat-extras.batman.overrideAttrs (_oldAttrs: {
-      doCheck = false;
-    });
-    batpipe = prev.bat-extras.batpipe.overrideAttrs (_oldAttrs: {
-      doCheck = false;
-    });
-  };
+  bat-extras = prev.bat-extras.overrideScope (
+    selfBat: _superBat: {
+      buildBatExtrasPkg =
+        selfBat.callPackage "${prev.path}/pkgs/tools/misc/bat-extras/buildBatExtrasPkg.nix"
+          {
+            nushell = null;
+          };
+      core =
+        (selfBat.callPackage "${prev.path}/pkgs/tools/misc/bat-extras/modules/core.nix" { }).overrideAttrs
+          (_oldAttrs: {
+            doCheck = false;
+            nativeCheckInputs = [ ];
+          });
+    }
+  );
 }
