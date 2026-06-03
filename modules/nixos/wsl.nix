595766df4e4f5d962c34f30239704a3514e82fa0
Keanu Kerr <keanu@kerr.us>
2026-06-03 03:56:31 +0000

    Update mars sops key

--- modules/nixos/wsl.nix
+++ modules/nixos/wsl.nix
@@ -10,19 +10,25 @@ let
     ;
 in
 {
-  flake.modules.nixos.wsl = {
-    imports = [
-      nix-settings
-      system-packages
-      sops
-      comin
-      tailscale
-      nh
-      inputs.wsl.nixosModules.default
-      inputs.nur.modules.nixos.default
-    ];
+  flake.modules.nixos.wsl =
+    { lib, ... }:
+    {
+      imports = [
+        nix-settings
+        system-packages
+        sops
+        comin
+        tailscale
+        nh
+        inputs.wsl.nixosModules.default
+        inputs.nur.modules.nixos.default
+      ];
 
-    wsl.enable = true;
-    wsl.defaultUser = "keanu";
-  };
+      wsl.enable = true;
+      wsl.defaultUser = "keanu";
+
+      # WSL does not have SSH host keys by default, so sops-nix cannot derive
+      # an age key from them. Use the user's SSH key instead.
+      sops.age.sshKeyPaths = lib.mkForce [ "/home/keanu/.ssh/id_ed25519" ];
+    };
 }
