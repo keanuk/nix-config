d5281c1d1846d9566cc4d9db94120584318f5ecd
Keanu Kerr <keanu@kerr.us>
2026-08-27 00:36:47 -0400

    Workaround to allow leetgo to read firefox cookies

--- modules/home/desktop/firefox/default.nix
+++ modules/home/desktop/firefox/default.nix
@@ -7,5 +7,8 @@
         package = pkgs.firefox-devedition;
         configPath = "${config.xdg.configHome}/mozilla/firefox";
       };
+
+      # workaround to allow leetgo to read Firefox cookie
+      home.file.".mozilla/firefox".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/mozilla/firefox";
     };
 }
